ac160466aca41794cb307bd8ccac04fc3bc5f82f
André Sousa <andreosousa95@gmail.com>
2017-12-05 11:08:58 -0100

    Update day5.go

--- day5.go
+++ day5.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 func main() {
+	file, err := os.Open("day5_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	
+	scanner := bufio.NewScanner(file)
 }
 
 func part1() {
