15397036addb13e4047d353cbd3f0a361d6ef00e
André Sousa <andreosousa95@gmail.com>
2017-12-06 19:29:59 -0100

    Day 6 solution

--- day6.go
+++ day6.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -22,10 +24,83 @@ func main() {
 	part2(numbersTwo)
 }
 
-func part1(nums []int) {
+func part1(nums []int) ([]int, []int) {
+	var compNums = make([][]int, 0)
+	var cycles = 0
 
+	for {
+		var max = 0
+		var maxIndex = 0
+		var prevNums = make([]int, len(nums))
+
+		// Find highest value in slice
+		for index, val := range nums {
+			if val > max {
+				max = val
+				maxIndex = index
+			}
+		}
+
+		nums[maxIndex] = 0
+
+		// Redistribute blocks
+		for i := 0; i < max; i++ {
+			maxIndex++
+			if maxIndex > len(nums)-1 {
+				maxIndex = 0
+			}
+			nums[maxIndex]++
+		}
+
+		// Check for a possible repeat
+		for _, value := range compNums {
+			if reflect.DeepEqual(nums, value) {
+				fmt.Println("Redistribution Cycles:", cycles+1)
+				return nums, value
+			}
+		}
+
+		copy(prevNums, nums)
+		compNums = append(compNums, prevNums)
+
+		cycles++
+	}
 }
 
 func part2(nums []int) {
+	nums, compNums := part1(nums)
 
+	var cycles = 0
+
+	for {
+		var max = 0
+		var maxIndex = 0
+
+		// Find highest value in slice
+		for index, val := range nums {
+			if val > max {
+				max = val
+				maxIndex = index
+			}
+		}
+
+		nums[maxIndex] = 0
+
+		// Redistribute values
+		for i := 0; i < max; i++ {
+			maxIndex++
+			if maxIndex > len(nums)-1 {
+				maxIndex = 0
+			}
+			nums[maxIndex]++
+		}
+
+		// Check for a possible repeat
+		if reflect.DeepEqual(nums, compNums) {
+			fmt.Println("Redistribution Cycles:", cycles+1)
+			return
+		}
+
+		cycles++
+	}
 }
